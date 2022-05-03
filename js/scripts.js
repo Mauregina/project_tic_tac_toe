2ba9ae1a92eb10f3e8cc3f4186f256b860e331bf
Mauregina <mauraregina@gmail.com>
2022-05-03 09:30:10 -0400

    implement logic to get to know the winner
    Signed-off-by: Mauregina <mauraregina@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -15,10 +15,7 @@ for(let i=0; i < boxes.length; i++){
             turn++
         }
 
-        if (turn > 4){ 
-            console.log('Jogo finalizado')
-            winner = hasWinner()
-        }
+        checkHasWinner(turn)
     })
 }
 
@@ -32,16 +29,33 @@ const createElement = (turn) => {
     return cloneElement
 }
 
-const hasWinner = () => {
-    const [indexesX, indexesO] = getIndexes()
+const checkHasWinner = (turn) => {
+    if (turn > 4){ 
+        let hasWinner = false
+        const [indexesX, indexesO] = getIndexes()
 
-    console.log(indexesX)
-    console.log(indexesY)
+        hasWinner = isCorrectCombination(indexesX)
+
+        if (hasWinner) {
+            console.log('Jogador 1 ganhou')
+        } else { 
+            hasWinner = isCorrectCombination(indexesO)
+
+            if (hasWinner) {
+                console.log('Jogador 2 ganhou')
+            } else {
+                if (turn == 9) {
+                    console.log('Velhou!') 
+                }
+            }
+        }
+    }
 }
 
 const getIndexes = () => {
     let indexesX = []
     let indexesO = [] 
+    
     boxes.forEach(function callback(value, index){
         if (value.childNodes[0]) {
             element = value.childNodes[0].className
@@ -53,6 +67,27 @@ const getIndexes = () => {
         } 
     })
     return [indexesX, indexesO]
+}
+
+const isCorrectCombination = (indexes) => {
+    let indexesStr = JSON.stringify(indexes) 
+
+    // Horizontal
+    if ((indexesStr == JSON.stringify([0,1,2])) || (indexesStr == JSON.stringify([3,4,5])) || (indexesStr == JSON.stringify([6,7,8]))) {
+        return true
+    }
+
+    // Vertical
+    if ((indexesStr == JSON.stringify([0,3,6])) || (indexesStr == JSON.stringify([1,4,7])) || (indexesStr == JSON.stringify([2,5,8]))) {
+        return true
+    }
+
+    // Diagonal
+    if ((indexesStr == JSON.stringify([0,4,8])) || (indexesStr == JSON.stringify([2,4,6]))){
+        return true
+    }
+
+    return false
 }
 
 
